a6b5357bb9cbc626abcef144fe3e9745bebdae6c
mukhamadrifki71-creator <mukhamadrifki71@guru.sd.belajar.id>
2026-09-22 09:25:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Menambahkan efek interaktif saat tombol di-klik
+document.getElementById('btnKlik').addEventListener('click', function() {
+    alert('Terima kasih sudah mengunjungi website saya!');
+});
